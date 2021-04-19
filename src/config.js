99cf1ed5b60642670ba07d9576b239eb3af17134
Gaein nidb <mail@gaein.cn>
2021-04-19 14:15:01 +0800

    Add config

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const dataUrl = "https://static.cdn.gaein.cn/website_used/error_page_data.json";
+const defaultMsg = {
+    };
+
+export default appConfig { dataUrl: dataUrl, defaultMsg: defaultMsg };
