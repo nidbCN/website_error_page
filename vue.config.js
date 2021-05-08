c48da988762de9cc5a02f36211a4ef444ac528ea
Gaein nidb <mail@gaein.cn>
2021-05-08 13:32:39 +0800

    Add build script

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-<<<<<<< HEAD
-=======
+  configureWebpack: {
+    mode: 'production',
+  },
   publicPath: '/error_pages',
->>>>>>> 540af4263cc949c249e9e32f3f96f4388dede020
   transpileDependencies: [
     'vuetify'
   ]
